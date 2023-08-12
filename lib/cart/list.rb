27956685286b182bd745d8590ac986115dc8b8ed
Xerxes Regulus <johnpauliloilo@gmail.com>
2023-08-12 19:46:29 +0800

    adds support for discounts for certain amount of total reached

--- lib/cart/list.rb
+++ lib/cart/list.rb
@@ -12,10 +12,11 @@ module Cart
         output_text += "Products in Shopping Cart: \n"
         output_text += "# | Product Name | Amount | Price \n"
         cart_items.each_with_index do |cart_item, index|
-          output_text += "#{index + 1} | #{cart_item['name']} | #{cart_item['amount']} | #{sprintf('%.2f', amount_price(cart_item['price'], cart_item['amount']))} \n"
+          output_text += "#{index + 1} | #{cart_item['name']} | #{cart_item['amount']} | $#{sprintf('%.2f', amount_price(cart_item['price'], cart_item['amount']))} \n"
         end
       end
 
+      output_text += total_amount(cart_items)
       output_text
     end
   end
@@ -25,4 +26,40 @@ module Cart
   def amount_price(price, amount)
     (price.to_f * amount.to_f).round(2)
   end
+
+  def total_amount(cart_items)
+    output_text = ''
+    total = cart_items.map { |cart_item| amount_price(cart_item['price'], cart_item['amount']) }.sum
+
+    if total > 20
+      output_text += apply_discount(total)
+    else
+      output_text += "Your total amount is #{total}"
+    end
+  end
+
+  def apply_discount(total)
+    discount = discount_amount(total)
+    discounted_total = deduct_percent_from_total(discount[:amount], total).round(2)
+
+    "\nDiscount Applied: #{discount[:amount] * 100}% off on total greater than $#{discount[:trigger]} \n\n" \
+    "TOTAL: $#{sprintf('%.2f', discounted_total)}"
+  end
+
+  def discount_amount(total)
+    if total > 100
+      { amount: 0.20, trigger: 100 }
+    elsif total > 50
+      { amount: 0.15, trigger: 50 }
+    elsif total > 20
+      { amount: 0.10, trigger: 20 }
+    else
+      { amount: 0, trigger: 0 }
+    end
+  end
+
+  def deduct_percent_from_total(percent, total)
+    total - (total * percent)
+  end
+
 end
