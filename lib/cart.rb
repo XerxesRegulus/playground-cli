d485628aedca193e5df394b970b02bfc200c39f9
Xerxes Regulus <johnpauliloilo@gmail.com>
2023-08-12 18:16:34 +0800

    move the cart add code to its own nested module Cart::Add

--- lib/cart.rb
+++ lib/cart.rb
@@ -1,34 +1,12 @@
 require 'json'
 
+require './lib/cart/add.rb'
+
 module Cart
-  extend self
   FILE_LOCATION = './cart.json'
 
-  def add(index)
-    return 'Invalid Product ID.' if index.to_i < 1
-
-    output_text = ''
-    product = Product.all[index.to_i - 1]
-
-    unless product.nil?
-      cart_items = parse_cart_contents
-      same_item = cart_items.select { |cart_item| cart_item['uuid'] == product['uuid'] }[0]
-
-      if same_item.nil?
-        cart_items << product.merge({"amount" => 1})
-      else
-        same_item_index = cart_items.find_index(same_item)
-        cart_items[same_item_index]['amount'] += 1
-      end
-
-      write_to_file(cart_items.to_json)
-      output_text = "#{product['name']} has been successfully added to the cart."
-    else
-      output_text = "Product not found. Please use the '-l' or '--list' option to list out the available products."
-    end
-
-    output_text
-  end
+  extend self
+  extend Cart::Add
 
   private
 
