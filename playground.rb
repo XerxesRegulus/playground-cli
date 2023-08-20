c0379682e31bd167af547aec8006d37f9a75851d
Xerxes Regulus <johnpauliloilo@gmail.com>
2023-08-20 19:44:29 +0800

    update the text on thr -r command, add an integer text for -a, add a checker for invalid cart index value

--- playground.rb
+++ playground.rb
@@ -7,13 +7,13 @@ OptionParser.new do |parser|
   parser.on('-l', '--list', 'lists the available products') do |l|
     puts Product.list
   end
-  parser.on('-a', '--add-to-cart PRODUCTNAME', "adds a product to the cart using the product's ID, you can find a product's id using --list") do |product_name|
-    puts Cart.add(product_name)
+  parser.on('-a', '--add-to-cart PRODUCTINDEX', Integer,"adds a product to the cart using the product's ID, you can find a product's id using --list") do |product_index|
+    puts Cart.add(product_index)
   end
   parser.on('-c', '--cart-list', 'lists the current items in your cart with a total') do |c|
     puts Cart.list
   end
-  parser.on('-r', '--remove-from-cart CARTINDEX', Integer, 'lists the current items in your cart with a total') do |ci|
+  parser.on('-r', '--remove-from-cart CARTINDEX', Integer, 'Removes a cart item based on its cart index') do |ci|
     puts Cart.remove(ci)
   end
 end.parse!
